50d13b14da45625eaa66d8e804cea881b70f3d18
Josh Quintus <josh.quintus@gmail.com>
2023-03-15 22:48:10 -0400

    moved neotrellis logic to dedicated class

--- code.py
+++ code.py
@@ -1,41 +1,19 @@
-# SPDX-FileCopyrightText: 2022 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
-import board
-from adafruit_neotrellis.neotrellis import NeoTrellis
 
 # needed for keycodes 
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
+# Needed for Edge Detection
+from adafruit_neotrellis.neotrellis import NeoTrellis
+
 # my imports
 from arcade_hid import ArcadeKeyboard
 from buttons import LambdaButton, EmptyButton, KeyComboButton, VolumeButton
+from trellis import Trellis
 
 print("Hello, CircuitPython!")
-
-# create the i2c object for the trellis
-i2c_bus = board.I2C()  # uses board.SCL and board.SDA
-trellis = NeoTrellis(i2c_bus)
-trellis.brightness = 0.12
-
-arcade = ArcadeKeyboard()
-arcade.start()
-k = arcade.get_keyboard()
-cc = arcade.get_consumer_control()
-
-# some color definitions
-OFF = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-
-# this will be called when neo-tesllis events are received
-def blink(event):
+def on_press(event):
     # Button Orientation If the Switch is on the top
     #  3  7 11 15
     #  2  6 10 14
@@ -51,6 +29,23 @@ def blink(event):
     # turn the LED off when a falling edge is detected
     elif event.edge == NeoTrellis.EDGE_FALLING:
         trellis.pixels[event.number] = OFF
+
+arcade = ArcadeKeyboard()
+arcade.start()
+k = arcade.get_keyboard()
+cc = arcade.get_consumer_control()
+
+trellis = Trellis(on_press)
+trellis.start()
+
+# some color definitions
+OFF = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
 
 buttons = [
         # Column 0
@@ -78,24 +73,7 @@ buttons = [
         VolumeButton(  "Volume Up",   cc, ConsumerControlCode.VOLUME_INCREMENT),
         ]
 
-for i in range(16):
-    # activate rising edge events on all keys
-    trellis.activate_key(i, NeoTrellis.EDGE_RISING)
-    # activate falling edge events on all keys
-    trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
-    # set all keys to trigger the blink callback
-    trellis.callbacks[i] = blink
-
-    # cycle the LEDs on startup
-    trellis.pixels[i] = PURPLE
-    time.sleep(0.05)
-
-for i in range(16):
-    trellis.pixels[i] = OFF
-    time.sleep(0.05)
-
 while True:
-
     arcade.wait_for_bluetooth_connection()
 
     while arcade.is_connected():
